151ea92f998f465b0065804327efc437c990d9a9
Sharon Tokiti Mshekero <sharontokiti@gmail.com>
2023-03-11 15:10:21 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Your code here
+const dodger = document.getElementById("dodger");
+document.addEventListener('keydown' , function(e){
+    if (e.key === 'ArrowLeft'){
+        moveDodgerLeft();
+    }
+      
+       if (e.key === 'ArrowRight'){
+        moveDodgerRight();
+       }
+})
+
+  function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace('px', '');
+    const  lefts = parseInt(leftNumbers, 10);
+
+    if ( lefts < 360) {
+      dodger.style.left = `${lefts + 4}px`;
+    }
+  }
+
+
+  function moveDodgerLeft(){
+    const leftNumber=dodger.style.left.replace("pr","");
+    const left=parseInt(leftNumber,10)
+
+   if(left > 0){
+    dodger.style.left=`${left - 1}px`;
+}
+  }
